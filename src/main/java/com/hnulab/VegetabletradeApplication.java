519e1e2b4a3fbf16c93443cade64093c8e0c746e
Wyd <1319886541@qq.com>
2017-12-06 11:26:47 +0800

    第一次提交 ---王耀东

---
+++ src/main/java/com/hnulab/VegetabletradeApplication.java
@@ -0,0 +1,12 @@
+package com.hnulab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegetabletradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegetabletradeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hnulab/VegetabletradeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hnulab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VegetabletradeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
