361fb2ed05d18eb0e6105f62254a43793c20cd35
Wyd <1319886541@qq.com>
2017-12-06 11:30:53 +0800

    IDEA提交测试-王耀东

--- src/main/java/com/hnulab/VegetabletradeApplication.java
+++ src/main/java/com/hnulab/VegetabletradeApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VegetabletradeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(VegetabletradeApplication.class, args);
+		System.out.println("IEDA提交测试--王耀东");
 	}
 }
